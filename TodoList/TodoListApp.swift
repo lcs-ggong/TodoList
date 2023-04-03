e21de788abb510d0b89cca30a56f0ebb20242469
Grace Gong <ggong@lcs.on.ca>
2023-04-03 15:15:01 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Grace Gong on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
